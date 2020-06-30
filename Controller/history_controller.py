ed97312b79ec5906d3cd303dfdc581853fd0a873
User <damarxrider@gmail.com>
2020-06-30 02:38:36 +0200

    added history_controller logic

--- Controller/history_controller.py
+++ Controller/history_controller.py
@@ -1,11 +1,54 @@
 from Controller import client_controller
 from Controller import market_controller as market
+from datetime import datetime as fucking_date
+import datetime
+import time
 
-product_history = {}
+product_history = []
+client = None
 
 
 def get_history():
-    # TODO https://docs.pro.coinbase.com/#get-historic-rates
-    for product in market.products:
-        print('wallet:')
-        print(product)
+    global client, product_history
+
+    # from docs
+    # [ time, low, high, open, close, volume  ],
+    # [ 1415398768, 0.32, 4.2, 0.35, 4.2, 12.3  ],
+    while 1:
+        if client == None:
+            client = client_controller.client
+        for product in market.products:
+            if len(product_history) > 0:
+                p_history = [
+                    x for x in product_history if x['id'] == product.id]
+            else:
+                p_history = []
+            try:
+                start_date = fucking_date.now() - \
+                    datetime.timedelta(336) if len(
+                        p_history) == 0 else fucking_date.fromtimestamp(p_history[0]['data'][-1][0])
+                end_date = start_date+datetime.timedelta(0, 21600*300)
+
+                if start_date > fucking_date.now():
+                    time.sleep(1800)
+                    continue
+                if end_date > fucking_date.now():
+                    end_date = fucking_date.now()
+
+                data = client.get_product_historic_rates(
+                    product.id, start=start_date, end=end_date, granularity=300)
+
+                if len(p_history) == 0 and len(data) > 1:
+                    data.reverse()
+                    p_history = {'id': product.id, 'data': data}
+                    product_history.append(p_history)
+                elif len(data) > 1:
+                    data.reverse()
+                    p_history[0]['data'].extend(data)
+            except Exception as e:
+                print('exception')
+                print(e)
+
+            time.sleep(1)
+        print(product_history)
+        time.sleep(10)
--- Models/transaction.py
+++ Models/transaction.py
@@ -10,5 +10,11 @@ class Transaction:
     fee = None
     rate = None
 
-    def __init__(self, id, base_currency, quote_currency, **kwargs):
-        pass
+    def __init__(self, id, base_currency, quote_currency, fee=None, base_value=None, quote_value=None, rate=None):
+        self.id = id
+        self.base_currency = base_currency
+        self.quote_currency = quote_currency
+        self.fee = fee
+        self.base_value = base_value
+        self.quote_value = quote_value
+        self.rate = rate
